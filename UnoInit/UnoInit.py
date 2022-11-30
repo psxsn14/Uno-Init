1c666257e2eb3fb35eb7467e31ef5418866d669c
psxsn14 <psxsn14@nottingham.ac.uk>
2022-11-30 11:11:19 +0000

    Handle player turn

--- UnoInit/UnoInit.py
+++ UnoInit/UnoInit.py
@@ -8,14 +8,16 @@
 # Please familiarize yourself with gitlab/gitkraken by Friday and use this .py to try new things as it already has an implementation of a shuffled deck.
 
 from ctypes.wintypes import VARIANT_BOOL
+from pickle import NONE
 import random
 import copy
 
 #Class handling the game.
 class Uno:
     drawPile = []
+    discardPile = []
     def __init__(self):
-        #Instance variables for the card groups.
+        #Instance variables for card groups.
         self.greenCards = []
         self.yellowCards = []
         self.blueCards = []
@@ -28,6 +30,8 @@ class Uno:
         self.playerList = []
         self.isGameOver = False
         self.currentGameColour = ""
+        #Top card from the discard pile that sets the current game colour.
+        self.topDiscardPileCard = None
     
     #Create a new shuffled deck.
     def createNewDeck(self):
@@ -101,34 +105,33 @@ class Uno:
         ###
 
     def startGame(self):
-        topCard = self.drawPile[0]
-        print(f"\nTop card of the draw pile forms the discard pile: {topCard}")
-        self.currentGameColour = topCard.cardColour
-        self.discardPile.append(topCard)
+        self.topDiscardPileCard = self.drawPile[0]
+        print(f"\nTop card of the draw pile forms the discard pile: {self.topDiscardPileCard}")
+        self.currentGameColour = self.topDiscardPileCard.cardColour
+        self.discardPile.append(self.topDiscardPileCard)
         self.drawPile.pop(0)
-        
-        self.drawPile, self.discardPile = self.playerList[0].playTurn(self.drawPile, self.currentGameColour)
+        self.drawPile, self.discardPile = self.playerList[0].playTurn(self.drawPile, self.currentGameColour, self.discardPile)
 
 
 #Class for handling Player activities.
-class Player(Uno):
+class Player():
     def __init__(self, playerNo, plDeck = []):
         super().__init__()
         self.playerNo = playerNo
         self.plDeck = plDeck
 
-    def playTurn(self, drawPile, currentGameColour):
+    def playTurn(self, drawPile, currentGameColour, discardPile):
         #Select a card from your hand, and place it below the discard pile.
-        self.numCards = len(self.plDeck)
         
         print(f"Player {self.playerNo} -- Cards on hand:\n")
         for i in self.plDeck:
             print(i)
 
         #Initiate special rule.
+        
         cardChoice = int(input("\nSPECIAL RULE: Select a card (1-7) from your hand and keep it beneath the discard pile..."))
         #Add player card to the discard pile.
-        self.discardPile.append(self.plDeck[cardChoice - 1])
+        discardPile.append(self.plDeck[cardChoice - 1])
         #Remove card from player's deck.
         self.plDeck.pop(0)
 
@@ -149,19 +152,45 @@ class Player(Uno):
         if(playerChoice == 0):
             self.plDeck.insert(0, drawPile[0])
             drawPile.pop(0)
-            return drawPile,self.discardPile
+            print("\nPlayer Deck after selecting card.....")                   
+            for i in self.plDeck:
+                print(i)
+
+            print("\nDiscard pile after selecting card.....")                   
+            for i in discardPile:
+                print(i)
+            print(f"\nPlayer {self.playerNo} turn complete.\n")
+            return drawPile, discardPile
         
         #If 1-n, validate the card and proceed.
         if(playerChoice in range(1, (len(self.plDeck) + 1))):
-            print("Correct card")
-
-            return drawPile,self.discardPile
-
-
+        
         #If colour is same but number is different or a special card, can play.
+            if(self.plDeck[playerChoice - 1].cardColour == currentGameColour):
+                #If normal, play.
+                if(self.plDeck[playerChoice - 1].cardType == "Normal"):
+                    #Make the selection as the top card of the discard pile and add it to discard pile.
+                    discardPile.append(self.plDeck[playerChoice - 1])
+                    self.plDeck.pop(self.plDeck.index(self.plDeck[playerChoice - 1]))
+                    print("Player Deck after selecting card.....")                   
+                    for i in self.plDeck:
+                        print(i)
+                    return drawPile, discardPile
+        
+                    #Remove card from player deck.
+
+                #If special, execute stuff.
+                 
         #If number is same, but colour is different, can play.
+        
         #If colour change, can play.
+        
         #If draw4, can only play if no matching colour card on hand.
+        
+        return drawPile,self.discardPile
+
+
+
          
     def __repr__(self):
         playerTemp = []
@@ -193,7 +222,6 @@ newGame = Uno()
 newGame.startPreGame(newGame.createNewDeck())
 
 newGame.startGame()
-
 
 
 
