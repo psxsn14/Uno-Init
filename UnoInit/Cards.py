8705341c8077099c27133ce5b2f9806e8f143eb4
pmyak11 <pmyak11@nottingham.ac.uk>
2022-12-03 17:49:53 +0000

    Linked card images with logic for deal function

--- UnoInit/Cards.py
+++ UnoInit/Cards.py
@@ -55,3 +55,34 @@ for i in range (0,2):
 for i in range (0,2):
     yellow.append(pygame.image.load('yellowDrawTwo'+ '.png'))
 
+    black = []
+        for i in range(0,4):
+            black.append(pygame.image.load('blackcolorchange.png'))
+        for i in range(0,4):
+            black.append(pygame.image.load('blackDrawFour.png'))
+        
+        listImages = green + red + blue + yellow + black
+
+for i in range(1,len(shuffledDeck)): 
+          UnoCard() = listImages[0]
+
+
+
+
+
+          start = True
+        count = 7
+  start = True
+        while start:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    start = False
+                for g in range(7):
+                    x = 300 + g*100
+                    count -= 1
+                    
+                    pygame.display.update()
+                    time.sleep(0.1)
+        pygame.quit() 
+                    
+            
