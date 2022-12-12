7e1cad1136d75b76fc866d6a380a82b179148dc0
Qingfei HU <psxqh5@exmail.nottingham.ac.uk>
2022-12-12 00:16:55 +0000

    hqf

--- UnoInit/WelcomePage.py
+++ UnoInit/WelcomePage.py
@@ -18,17 +18,17 @@ class Button():
             self.image=self.text
         self.rect=self.image.get_rect(center=(self.x_pos,self.y_pos))
         self.text_rect=self.text.get_rect(center=(self.x_pos,self.y_pos))
-        
+
     def update(self, screen):
         if self.image is not None:
             screen.blit(self.image, self.rect)
         screen.blit(self.text,self.text_rect)
-    
+
     def checkForInput(self, position):
         if position[0] in range(self.rect.left, self.rect.right)and position[1]in range(self.rect.top,self.rect.bottom):
             return True
         return False
-    
+
     def changeColor(self,position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
             self.text=self.font.render(self.text_input, True, self.hovering_color)
@@ -37,17 +37,17 @@ class Button():
 
 def chooselevel():
     pygame.display.set_caption("chooselevel")
-    
+
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-    
+
         bgImage = pygame.image.load('blue_BG.jpg')
         SCREEN.blit(bgImage, (0,0))
-        
+
         Main_TEXT = get_font(45).render("Choose the level of games", True, "white")
         Main_RECT = Main_TEXT.get_rect(center=(683, 84))
         SCREEN.blit(Main_TEXT, Main_RECT)
-    
+
         PLAY_BACK = Button(image=None, pos=(683,660),
                      text_input="BACK", font=get_font(55), base_color="White", hovering_color="Green")#悬停的颜色
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
@@ -74,7 +74,7 @@ def chooselevel():
         InvincibleAI_LEVEL.update(SCREEN)
 
 
-        
+
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
@@ -94,22 +94,22 @@ def chooselevel():
                 if InvincibleAI_LEVEL.checkForInput(PLAY_MOUSE_POS):
                     AIlevel="InvincibleAI"
                     chooseAIplayer()
-                    
+
         pygame.display.update()
 
 def chooseAIplayer():
     pygame.display.set_caption("chooseAIplayer")
-    
+
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
-    
+
         bgImage = pygame.image.load('blue_BG.jpg')
         SCREEN.blit(bgImage, (0,0))
-        
+
         Main_TEXT = get_font(45).render("Choose the number of AIplayers", True, "white")
         Main_RECT = Main_TEXT.get_rect(center=(683, 84))
         SCREEN.blit(Main_TEXT, Main_RECT)
-    
+
         PLAY_BACK = Button(image=None, pos=(683,660),
                      text_input="BACK", font=get_font(55), base_color="White", hovering_color="Green")#悬停的颜色
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
@@ -130,7 +130,7 @@ def chooseAIplayer():
         Player3.changeColor(PLAY_MOUSE_POS)
         Player3.update(SCREEN)
 
-        
+
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
@@ -140,7 +140,7 @@ def chooseAIplayer():
                     main_menu()
                 if Player1.checkForInput(PLAY_MOUSE_POS):
                     AIplayers = 1
-        
+
                     newGame = Uno()
 
                     newGame.startPreGame(newGame.createNewDeck(), SCREEN)
@@ -164,33 +164,34 @@ def chooseAIplayer():
                     newGame.startPreGame(newGame.createNewDeck(), SCREEN)
                     pass
 
-                
+
         pygame.display.update()
 
 def rules():
     pygame.display.set_caption("RULES")
-    
-    
+
+
     while True:
         RULES_MOUSE_POS = pygame.mouse.get_pos()
-    
+
         bgImage = pygame.image.load('blue_BG.jpg')
         SCREEN.blit(bgImage, (0,0))
         rulesImage = pygame.image.load('UnoRules.png')
         rulesRect = rulesImage.get_rect(center = (1366/2, 768/2))
         SCREEN.blit(rulesImage,rulesRect)
-        
+
         # RULES_TEXT = get_font(45).render("This is the Rules screen", True, "white")
         # RULES_RECT = RULES_TEXT.get_rect(center=(640, 260))
         # SCREEN.blit(RULES_TEXT, RULES_RECT)
-    
-        RULES_BACK = Button(image=None, pos=(1200,700),text_input="BACK", font=get_font(45), base_color="White", hovering_color="Green")#悬停的颜色        
-        #PLAY_BUTTON=Button(image=None, pos=(450,710),text_input="PLAY",font=get_font(45), base_color="White", hovering_color="Green")
-        
+
+        RULES_BACK = Button(image=None, pos=(1200,700),text_input="BACK", font=get_font(45), base_color="White", hovering_color="Green")#悬停的颜色
+        PLAY_BUTTON=Button(image=None, pos=(450,710),text_input="PLAY",font=get_font(45), base_color="White",
+        hovering_color="Green")
+
         for button in [PLAY_BUTTON,RULES_BACK]:
             button.changeColor(RULES_MOUSE_POS)
             button.update(SCREEN)
-        
+
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
@@ -200,8 +201,8 @@ def rules():
                     main_menu()
                 #if PLAY_BUTTON.checkForInput(RULES_MOUSE_POS):
                     #chooselevel()
-                
-        pygame.display.update()   
+
+        pygame.display.update()
 
 
 # from button import Button
@@ -221,34 +222,34 @@ color = (255,255,255)
 music=pygame.mixer.music.load('welcome page/BGmusic.mp3')
 pygame.mixer.music.play(-1)
 def get_font(size): # Returns Press-Start-2P in the desired size
-    return pygame.font.Font("welcome page/font1.ttf", size)  
+    return pygame.font.Font("welcome page/font1.ttf", size)
 
 def main_menu():
     pygame.display.set_caption("Menu")
 
     while True:
         #SCREEN.fill((70,130,180))#BG color
-        
+
         bgImage = pygame.image.load('blue_BG.jpg')
         SCREEN.blit(bgImage, (0,0))
 
         SCREEN.blit(Cardimage,(330,30))#pho
-    
+
         MENU_MOUSE_POS = pygame.mouse.get_pos()
-    
+
     # MENU_TEXT=get_font(100).render("MAIN MENU",True,"#b68f40")
     # MENU_RECT=MENU_TEXT.get_rect(center=(640,100))
-                
+
         PLAY_BUTTON=Button(image=None, pos=(683,550),text_input="PLAY",font=get_font(45), base_color="#d7fcd4", hovering_color="white")
         RULES_BUTTON=Button(image=None, pos=(683,640),text_input="RULES",font=get_font(45), base_color="#d7fcd4", hovering_color="white")
         QUIT_BUTTON=Button(image=None, pos=(683,730),text_input="QUIT",font=get_font(45), base_color="#d7fcd4", hovering_color="white")
-                
+
     # SCREEN.blit(MENU_TEXT, MENU_RECT)
-  
+
         for button in [PLAY_BUTTON,RULES_BUTTON,QUIT_BUTTON]:
              button.changeColor(MENU_MOUSE_POS)
              button.update(SCREEN)
-                
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
